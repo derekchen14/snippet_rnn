1629aecc44c67d99a01864ab745ce8772a9c027d
Derek Chen <derekchen14@gmail.com>
2016-05-08 17:03:23 -0700

    new search for hyperparams with optimizer set to adam and recurrent dropout sent to False

--- classify_snippets.py
+++ classify_snippets.py
@@ -12,20 +12,21 @@ from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.layers.embeddings import Embedding
-from keras.callbacks import Callback
+from keras.callbacks import Callback, EarlyStopping
 from keras.layers.recurrent import LSTM, SimpleRNN, GRU
 from keras.preprocessing.sequence import pad_sequences
 
-# env = 'local'
+env = 'test'
 np.random.seed(14)
 batch_size = 100 if env == 'local' else 32
-epochs = 2 if env == 'local' else 10
+epochs = 7 if env == 'local' else 15
 
 cell_type = ['LSTM', 'GRU'] # https://arxiv.org/abs/1412.3555
 recurrent_dropout = [True, False] # http://arxiv.org/abs/1409.2329
 input_gate_dropout = [0.1, 0.3, 0.5] # http://arxiv.org/abs/1512.05287
 optimizers = ['adam', 'rmsprop'] # http://arxiv.org/abs/1412.6980
 cells_per_layer = [64, 128, 256] # https://github.com/karpathy/char-rnn
+multilayer_rnn = [True, False]
 
 def vectorize(X, y, word_idx, max_snippet_len):
   X_vectors = map(lambda (snippet): [word_idx[w] for w in snippet], X)
@@ -39,22 +40,23 @@ def vectorize(X, y, word_idx, max_snippet_len):
 
 def build_combinations():
   combos = []
-  # Not pretty, but effective
+  # for flag in recurrent_dropout:
+  # for optimizer in optimizers:
   for cell in cell_type:
-    for flag in recurrent_dropout:
-      for step in input_gate_dropout:
-        for optimizer in optimizers:
-          for count in cells_per_layer:
-            combos.append([cell, flag, step, optimizer, count])
-  return combos
-
+    for step in input_gate_dropout:
+      for count in cells_per_layer:
+        for layer in multilayer_rnn:
+          combos.append([cell, step, count, layer])
+  return random.sample(combos, 50)
 
 print('Loading data...')
 initial_time = tm.time()
 data = pd.read_pickle('data/training_data.p')
 if env == 'local':
   data = data[0:5000]
-data = data[0:9000]
+  cells_per_layer = [64, 80, 96]
+elif env == 'test':
+  data = data[0:10000]
 X_raw = data['Content'].tolist()
 y_raw = data['Tags'].tolist()
 print "------------- Loaded in %0.2fs ---------------" % (tm.time() - initial_time)
@@ -69,33 +71,6 @@ word_idx = dict((word, i + 1) for i, word in enumerate(vocab))
 X_train, y_train = vectorize(X_raw, y_raw, word_idx, max_snippet_len)
 print "------------- Prepared in %0.2fs ---------------" % (tm.time() - checkpoint)
 
-'''
-print('Vocabulary size: %d unique words') % vocab_size
-print('Max snippet length: %d words') % max_snippet_len
-print('Number of training inputs: %d') % len(X_raw)
-print('Number of training labels: %d') % len(y_raw)
-
-num = np.random.randint(0,5000)
-print('Here\'s a random snippet with its label and vectors (%d):') % num
-print X_raw[num]
-print y_raw[num]
-print X_train[num]
-print y_train[num]
-
-print len(y_train)
-print y_train[4:6]
-print len(y_train[6])
-print type(y_train[6])
-
-class show_results(Callback):
-  def on_train_begin(self, logs={}):
-      self.losses = []
-  def on_batch_end(self, batch, logs={}):
-      self.losses.append(logs.get('val_loss'))
-
-callback = show_results()
-'''
-
 combinations = build_combinations()
 
 for combo in combinations:
@@ -104,22 +79,24 @@ for combo in combinations:
   model = Sequential()
   model.add(Embedding(vocab_size, 128, input_length=max_snippet_len, dropout=0.2))
 
-  drop_u = 0.2 if combo[1] else 0.0 # for dropout on recurrent connections
-  drop_w = combo[2]  # for dropout on input gate connections
-
   if combo[0] == 'LSTM':
-    model.add(LSTM(combo[4], dropout_W=drop_w, dropout_U=drop_u))
+    if combo[3]:
+      model.add(LSTM(combo[2], return_sequences=True, dropout_W=combo[1]))
+    model.add(LSTM(combo[2], dropout_W=drop_w))
   elif combo[0] == 'GRU':
-    model.add(GRU(combo[4], dropout_W=drop_w, dropout_U=drop_u))
+    if combo[3]:
+      model.add(GRU(combo[2], return_sequences=True, dropout_W=combo[1]))
+    model.add(GRU(combo[2], dropout_W=drop_w))
+
   model.add(Dense(21))
   model.add(Activation('softmax'))
   model.compile(loss='categorical_crossentropy',
-      optimizer=combo[3], metrics=['accuracy'])
+      optimizer='adam', metrics=['accuracy'])
   print "------------- Model compiled in %0.2fs ---------------" % (tm.time() - checkpoint)
-  print("Cell type: %s" % combo[0], "Drop Recurrent: %s" % combo[1], "Drop Input: %s" % combo[2], "Optimizer: %s" % combo[3], "Layers: %s" % combo[4])
+  print("Cell type: %s" % combo[0], "Drop Input: %s" % combo[1], "Cell Count: %s" % combo[2], "Layers: %s" % combo[3])
 
   print('Training phase ...')
   model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=epochs,
           validation_split=0.2, verbose=2)
-          # validation_split=0.2, callbacks=[callback], verbose=2)
+  model = None
   print " "
